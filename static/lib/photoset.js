896e7b736d998a6a13e63871b28a3f120e54ea4d
amokrushin <anton@mokr.org>
2018-04-12 22:24:15 +0300

    reformat

--- static/lib/photoset.js
+++ static/lib/photoset.js
@@ -2,36 +2,36 @@
 
 /* globals define, require, $, app, socket, config, ajaxify, RELATIVE_PATH, utils */
 
-(function() {
-    require( ['photoswipe-loader', 'forum/topic/posts'], function( photoswipe, Posts ) {
+(function () {
+    require(['photoswipe-loader', 'forum/topic/posts'], function (photoswipe, Posts) {
         /* Disable default wrapping the images in links */
-        Posts.wrapImagesInLinks = function() {};
+        Posts.wrapImagesInLinks = function () {};
 
-        $( window )
-            .on( 'action:topic.loaded', function() {
-                $( '[component="post"]' ).each( function() {
-                    photoswipe.post( $( this ) );
-                } );
+        $(window)
+            .on('action:topic.loaded', function () {
+                $('[component="post"]').each(function () {
+                    photoswipe.post($(this));
+                });
 
-            } )
-            .on( 'action:posts.loaded', function( e, data ) {
-                data.posts.forEach( function( post ) {
-                    photoswipe.post( $( '[component="post"][data-pid="' + post.pid + '"]' ) );
-                } );
-            } )
-            .on( 'action:posts.edited', function( e, data ) {
-                photoswipe.post( $( '[component="post"][data-pid="' + data.post.pid + '"]' ) );
-            } )
-            .on( 'action:composer.preview', function() {
-                photoswipe.preview( $( '.composer-container .preview' ) );
-            } );
-    } );
+            })
+            .on('action:posts.loaded', function (e, data) {
+                data.posts.forEach(function (post) {
+                    photoswipe.post($('[component="post"][data-pid="' + post.pid + '"]'));
+                });
+            })
+            .on('action:posts.edited', function (e, data) {
+                photoswipe.post($('[component="post"][data-pid="' + data.post.pid + '"]'));
+            })
+            .on('action:composer.preview', function () {
+                photoswipe.preview($('.composer-container .preview'));
+            });
+    });
 
-    $( window ).on( 'action:ajaxify.contentLoaded', function( event, data ) {
-        if( data.tpl !== 'topic' || $( '.pswp' ).length ) return;
-        templates.parse( 'photoswipe', {}, function( html ) {
-            $( 'body' ).append( html );
-        } );
-    } );
+    $(window).on('action:ajaxify.contentLoaded', function (event, data) {
+        if (data.tpl !== 'topic' || $('.pswp').length) return;
+        templates.parse('photoswipe', {}, function (html) {
+            $('body').append(html);
+        });
+    });
 }());
 
